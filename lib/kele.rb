4075a1c757d5100bf42999df7543290dc768d1d9
summerlove91 <gkdus3040@gmail.com>
2017-09-01 19:25:44 -0700

    Retrieve Users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,13 +1,19 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
-  base_uri 'https://www.bloc.io/api/v1/sessions'
+  base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
     raise "Invalid email or password" if response.code == 404
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
   end
 
 
